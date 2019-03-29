4c0bc5c32ac34f1dccd067d3782cc904ecc9a015
Carly Pecora <carlyannpecora@gmail.com>
2019-03-29 00:56:25 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,14 @@
-//define functions here
+function getIt(){
+  $("p").on("click", function (){
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $(".tasty").on("load", function(){
+    this.style.border = "red"
+  })
+}
 
 $(document).ready(function(){
 
